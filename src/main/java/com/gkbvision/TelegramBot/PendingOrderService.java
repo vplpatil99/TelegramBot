acb0c10453c58a99d5057259b18a1f903dac83cc
Vipul ATGKB <vipul.patil@primelenses.com>
2026-01-13 12:25:04 +0530

    working

--- src/main/java/com/gkbvision/TelegramBot/PendingOrderService.java
+++ src/main/java/com/gkbvision/TelegramBot/PendingOrderService.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
 public class PendingOrderService {
 
 
